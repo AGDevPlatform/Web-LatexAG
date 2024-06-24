c02cf85c200f09be933f5c5c8473528de7e04704
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-06-24 13:27:34 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 const App = () => {
   const [inputText, setInputText] = useState("");
   const inputRef = useRef(null);
-
   const handleInputChange = (event) => {
     setInputText(event.target.value);
   };
@@ -19,6 +18,10 @@ const App = () => {
     { name: "Sum", formula: "\\sum_{i=1}^{n} x_i" },
     { name: "Product", formula: "\\prod_{i=1}^{n} x_i" },
     { name: "Square", formula: "x^2" },
+    { name: "Square Root", formula: "\\sqrt{x}" },
+    { name: "Fraction", formula: "\\frac{x}{y}" },
+    { name: "Square Root", formula: "\\sqrt{x}" },
+    { name: "Fraction", formula: "\\frac{x}{y}" },
     { name: "Square Root", formula: "\\sqrt{x}" },
     { name: "Fraction", formula: "\\frac{x}{y}" },
     { name: "Square Root", formula: "\\sqrt{x}" },
@@ -75,302 +78,140 @@ const App = () => {
   };
 
   return (
-    <div style={{ padding: "20px", backgroundColor: "#f3f3f3" }}>
-      <div style={{ marginBottom: "20px" }}>
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: "10px",
-            marginBottom: "10px",
-          }}
-        >
-          <div className="grid grid-rows-1 grid-flow-col gap-2">
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>{" "}
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>{" "}
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>{" "}
-            <div
-              className="grid grid-cols-3 gap-0 p-1"
-              style={{ borderWidth: "2px" }}
-            >
-              {basicFormulas1.map((item, index) => (
-                <div key={index} className="relative inline-block mr-1">
-                  <button
-                    onClick={() => insertFormula(item.formula)}
-                    style={{ borderRadius: "5px" }}
-                    className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
-                  >
-                    <img
-                      src={
-                        "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
-                      }
-                      alt="dummy-image"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
-                      }}
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>
+    // <div style={{ padding: "20px", backgroundColor: "#f3f3f3" }}>
+    //   <nav className="fixed shadow-md top-0 left-0 w-full z-10 bg-white border-b border-gray-200 backdrop-blur-sm bg-white/30">
+    //     <div style={{ marginBottom: "20px" }}>
+    //       <div
+    //         style={{
+    //           display: "flex",
+    //           flexWrap: "wrap",
+    //           gap: "10px",
+    //           marginBottom: "10px",
+    //         }}
+    //       >
+    //         <div className="grid grid-rows-1 grid-flow-col gap-2">
+    //           <div
+    //             className="grid grid-cols-3 gap-0 p-1"
+    //             style={{ borderWidth: "2px" }}
+    //           >
+    //             {basicFormulas1.map((item, index) => (
+    //               <div key={index} className="relative inline-block mr-1">
+    //                 <button
+    //                   onClick={() => insertFormula(item.formula)}
+    //                   style={{ borderRadius: "5px" }}
+    //                   className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
+    //                 >
+    //                   <img
+    //                     src={
+    //                       "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
+    //                     }
+    //                     alt="dummy-image"
+    //                     style={{
+    //                       width: "100%",
+    //                       height: "100%",
+    //                       objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
+    //                     }}
+    //                   />
+    //                 </button>
+    //               </div>
+    //             ))}
+    //           </div>
+    //           <div
+    //             className="grid grid-cols-3 gap-0 p-1"
+    //             style={{ borderWidth: "2px" }}
+    //           >
+    //             {basicFormulas1.map((item, index) => (
+    //               <div key={index} className="relative inline-block mr-1">
+    //                 <button
+    //                   onClick={() => insertFormula(item.formula)}
+    //                   style={{ borderRadius: "5px" }}
+    //                   className="w-9 h-9 bg-gray border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5"
+    //                 >
+    //                   <img
+    //                     src={
+    //                       "https://res.cloudinary.com/decwrwfpa/image/upload/v1719205399/img024arrows_zyowgf.svg"
+    //                     }
+    //                     alt="dummy-image"
+    //                     style={{
+    //                       width: "100%",
+    //                       height: "100%",
+    //                       objectFit: "contain", // Đảm bảo toàn bộ hình ảnh nằm gọn trong kích thước của nút
+    //                     }}
+    //                   />
+    //                 </button>
+    //               </div>
+    //             ))}
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </nav>
+
+    //   <div className="grid grid-cols-2 gap-4 h-screen border border-gray-300  divide-x divide-solid divide-black divide-w-2">
+    //     <div className="h-full">
+    //       <textarea
+    //         ref={inputRef}
+    //         value={inputText}
+    //         onChange={handleInputChange}
+    //         placeholder="Enter LaTeX formula"
+    //         className="w-full h-full p-4  text-base mb-4 outline-none"
+    //       />
+    //     </div>
+    //     <div
+    //       className="h-full p-4 mb-4 text-lg font-medium custom-font-output overflow-auto"
+    //       style={{ backgroundColor: "white" }}
+    //     >
+    //       <div
+    //         style={{
+    //           overflowWrap: "break-word",
+    //           wordWrap: "break-word",
+    //           whiteSpace: "pre-wrap",
+    //           textAlign: "justify",
+    //         }}
+    //       >
+    //         <Latex>{inputText}</Latex>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="p-5 bg-gray-200">
+      <div className="grid grid-cols-[auto,1fr,1fr] gap-4 h-screen border border-gray-300 divide-x divide-solid divide-black">
+        <div className="h-full grid grid-rows-4 gap-4 ">
+          <div
+            className="grid grid-rows-4 grid-flow-col gap-1 p-2"
+            style={{ borderWidth: "1px", margin: "5px" }}
+          >
+            {basicFormulas1.map((item, index) => (
+              <div key={index} className="relative inline-block">
+                <button
+                  onClick={() => insertFormula(item.formula)}
+                  className="w-9 h-9 bg-gray-300 border border-transparent hover:bg-blue-100 hover:border-blue-200 transition-colors duration-300 p-0.5 rounded"
+                >
+                  <img
+                    src="https://res.cloudinary.com/decwrwfpa/image/upload/v1719209406/img010operators_lmm7mj.svg"
+                    alt="dummy-image"
+                    className="w-full h-full object-contain"
+                  />
+                </button>
+              </div>
+            ))}
           </div>
+          <div>Heloo</div>
+          <div>Heloo</div>
+          <div>Heloo</div>
+          <div>Heloo</div>
         </div>
-      </div>
-      <div className="grid grid-cols-2 gap-4 h-screen border border-gray-300  divide-x divide-solid divide-black divide-w-2">
         <div className="h-full">
           <textarea
             ref={inputRef}
             value={inputText}
             onChange={handleInputChange}
             placeholder="Enter LaTeX formula"
-            className="w-full h-full p-4  text-base mb-4 outline-none"
+            className="w-full h-full p-4 text-base mb-4 outline-none"
           />
         </div>
-        <div
-          className="h-full p-4 mb-4 text-lg font-medium custom-font-output overflow-auto"
-          style={{ backgroundColor: "white" }}
-        >
-          <div
-            style={{
-              overflowWrap: "break-word",
-              wordWrap: "break-word",
-              whiteSpace: "pre-wrap",
-              textAlign: "justify",
-            }}
-          >
+        <div className="h-full p-4 mb-4 text-lg font-medium custom-font-output overflow-auto bg-white">
+          <div className="break-words whitespace-pre-wrap text-justify">
             <Latex>{inputText}</Latex>
           </div>
         </div>
